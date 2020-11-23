3cc44ecbc78083252ec1757c1245ba166c5b53d0
jacksonLai59 <jiechun.lai@gmail.com>
2020-11-23 16:00:04 +0800

    Initial Commit

---
+++ crud/crudApp.swift
@@ -0,0 +1,17 @@
+//
+//  crudApp.swift
+//  crud
+//
+//  Created by User07 on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct crudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
